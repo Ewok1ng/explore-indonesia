20270e1d41093b7332861bd3cd5af1f87963afb3
Ewok1ng <joker187566@gmail.com>
2023-07-16 00:37:51 +0300

    first commit

---
+++ src/js/index.js
@@ -0,0 +1,8 @@
+import { heroSlider } from './modules/heroSlider';
+import { exploreSlider } from './modules/exploreSlider';
+
+// SECTION hero-slider
+heroSlider();
+
+// SECTION explore-numbers
+exploreSlider();
